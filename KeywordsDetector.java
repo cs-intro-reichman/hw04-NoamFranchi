4affd2d3f6f30dc94223f182f6a4d604fcb71935
Noam Franchi <noam@Noams-MacBook-Air.local>
2025-11-25 20:48:58 +0200

    done

--- KeywordsDetector.java
+++ KeywordsDetector.java
@@ -21,6 +21,16 @@ public class KeywordsDetector {
     // Iterates through all the sentences.
     // If a sentence contains one or more of the kewords, prints it.
     public static void detectAndPrint(String[] sentences, String[] keywords) {
-        // Replace this comment with your code
+        for(int i = 0; i < sentences.length; i++){
+            sentences[i] = sentences[i].toLowerCase();
+            for(int j = 0; j < keywords.length; j++){
+                keywords[j] = keywords[j].toLowerCase();
+                if(MyString.contains(sentences[i], keywords[j])){
+                    System.out.println(sentences[i]);
+                    sentences[i] = "";
+                }
+            }
+        }
+
     }
 }
--- Primes.java
+++ Primes.java
@@ -1,5 +1,36 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        int n = Integer.parseInt(args[0]);
+        boolean[] primeNums = new boolean[n+1];
+        int p = 2;
+        while(p <= n){
+            primeNums[p] = true;
+            p++;
+        }
+        p = 2;
+        while(p <= Math.sqrt(n)){
+            if(primeNums[p]){
+                int k = p * p;
+                while(k <= n){
+                    primeNums[k] = false;
+                    k += p;
+
+                }
+            }
+            p++;
+        }
+        int i = 2;
+        int count = 0;
+        while (i < primeNums.length) {
+            if(primeNums[i]){
+
+                System.out.println(i);
+                count ++;
+            }
+            i++; 
+        }
+        double precent = (count / (double)n) * 100;
+        System.out.println("There are " + count + " primes between 2 and " + n + " (" + precent + "% are primes)");
+
     }
 }
